6d63c2159579993006ef0a1174ceb07c34afe56d
Morten Aslak <moaslak@gmail.com>
2022-09-06 12:51:13 +0200

    now with history

--- Scripts/JSCalc.js
+++ Scripts/JSCalc.js
@@ -4,6 +4,73 @@ let size = 6;
 let list = [];
 let selectSize = "";
 
+function inputFromSelect(){
+    var selectedString = document.getElementById("resultsBox");
+    //var a, b, result,
+    var operator = " ";
+    var index;
+    var temp = "";
+    for(var i = 0; i < selectedString.value.length; i++){
+        index = i;
+        
+        if(selectedString.value.charAt(i) != " "){
+            temp = temp + selectedString.value.charAt(i);
+            a = temp;    
+        }
+        else
+            break;
+    }
+    document.getElementById("a").value = a;
+    for(var i = index; i < selectedString.value.length; i++){
+        if(selectedString.value.charAt(i) == "+"){
+            operator = "+";
+                index = i;
+                break;
+        }
+        if(selectedString.value.charAt(i) == "-"){
+            operator = "-";
+                index = i;
+                break;
+        }
+        if(selectedString.value.charAt(i) == "*"){
+            operator = "*";
+                index = i;
+                break;
+        }
+        if(selectedString.value.charAt(i) == "/"){
+            operator = "/";
+                index = i;
+                break;
+        }
+    }
+    document.getElementById("operator").innerHTML = operator;
+    temp = "";
+    for(var i = index+2; i < selectedString.value.length; i++){
+        if(selectedString.value.charAt(i) != " "){
+            temp = temp + selectedString.value.charAt(i);
+            b = temp;    
+        }
+        else{
+            index = i;
+            break;
+        }      
+    }
+    document.getElementById("b").value = b;
+    index = index + 3;
+    temp = "";
+    for(var i = index; i < selectedString.value.length; i++){
+        if(selectedString.value.charAt(i) != " "){
+            temp = temp + selectedString.value.charAt(i);
+            result = temp;    
+        }
+        else{
+            index = i;
+            break;
+        }
+    }
+    document.getElementById("result").value = result;
+}
+
 function resultToTextarea(resultString){
     count++;
     let resultToBox = "";
@@ -16,19 +83,19 @@ function resultToTextarea(resultString){
     else{
         list.unshift(resultString);
     }
-    //TODO: select MAX 5
-    for(var i = 0; i < count; i++){
-        resultToBox = resultToBox + list[i];
-        document.getElementById("resultsBox").value = resultToBox;
-        document.getElementById("result").value = result;
-    }
-    selectSize = count;
-    //document.getElementById("resultsBox").size("5");
+    //document.getElementById("result").value = result;
 
-    var li = document.createElement('option');
-    li.value = resultString;
-    li.text = resultString;    
-    document.getElementById("resultsBox").options.add(li);
+    document.getElementById("resultsBox").innerHTML = "";
+
+    for(var i = 0; i < list.length; i++){
+        var li = document.createElement('option');
+        li.value = list[i];
+        li.text = list[i];    
+        document.getElementById("resultsBox").options.add(li);
+    }
+    inputFromSelect();
+    var sel = document.getElementById("resultsBox");
+    var selText = sel.value;
 }
 
 function Add(){
@@ -96,6 +163,9 @@ function Clear(){
         list.pop()
         count--;
     }
-        
+    document.getElementById("operator").innerHTML = "";
+    document.getElementById("a").value = 0;
+    document.getElementById("b").value = 0;
+    document.getElementById("result").value = 0;    
     document.getElementById("resultsBox").value = list;
 }
